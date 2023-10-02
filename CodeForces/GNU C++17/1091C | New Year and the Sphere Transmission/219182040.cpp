11bbc6c402dc07acef9eafb9b8270abe273c0635
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2023-10-02 21:32:26 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1091C | New Year and the Sphere Transmission/219182040.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+using namespace std;
+ 
+#define int long long
+ 
+int getsum(int x, int d) {
+ int l = 1;
+ int r = (x - 1) / d * d + 1;
+ int n = (r - l) / d + 1;
+ return ((r + l) * n) >> 1LL;
+}
+ 
+signed main() {
+ int n;
+ cin >> n;
+ 
+ set<int> st;
+ st.emplace(1);
+ for (int i = 2; i*i <= n; ++i) {
+  if (n % i == 0) {
+   st.emplace(getsum(n, i));
+   st.emplace(getsum(n, n/i));
+  }
+ }
+ st.emplace(n * (n+1) >> 1);
+ 
+ for (int x: st) {
+  cout << x << ' ';
+ }
+}
